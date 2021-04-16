f7e2ae101cb1a1ebf1fbe2062a4850930156a999
Victor Wekesa <wekae.dev@gmail.com>
2021-04-16 04:05:55 +0300

    Initial commit
    Initial commit with MDBBootrap added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  winesApiUrl: 'https://storage.googleapis.com/wineshop-assets/wine-shop.json',
+  imagesApiUrl: 'https://storage.googleapis.com/wineshop-assets/wine-bottles/'
+};
